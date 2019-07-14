9f67b7f7149fd495e69b0111ff0f4a336ca28445
Vitaly Chemezov <vitaly.chemezov@gmail.com>
2019-07-14 22:38:46 +0200

    add showStar page

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,10 @@ import React from "react";
 
 function Search(props) {
   const [searchMusic, setSearchMusic] = React.useState("");
-  const [searchList, setSearchList] = React.useState("");
+  const [searchList, setSearchList] = React.useState([""]);
   const [showStar, setShowStar] = React.useState("");
   const [imageSrc, setImageSrc] = React.useState("");
+  const [linkSrc, setLinkSrc] = React.useState("");
 
   async function handleInputChange(event) {
     //setSearchList([...searchList, value]);
@@ -17,52 +18,81 @@ function Search(props) {
         {
           headers: {
             Authorization:
-              "Bearer BQC-UTKgMvKHAOHYVhTraJlMC-Y2IxGr6IKwr2cT5loFVNwokl1pdI5cEca5yE0HaOyunw3KMT9SyHRDIUo7JlFzagQL1lS04Ismgg8730slPzzBRTU-wvk9paj2teuws5oKiD3-QImKSzS4"
+              "Bearer BQAY6kZzsYqqgdxPVUO4WTH6ApovTUNc2Ujp-VAHdOpRHl18SNVKR6WlUTlo20GwdNfF1chZoxcVyDRtvjKQ3eGfH4l2GE2Mz7LjeOja9N7hQnBRbv-ulKAc33li8YvfkH_-BJ0MBPZm7pOT"
           }
         }
       );
       let data = await res.json();
       const newFoundItem = data.artists.items[0].name;
       setSearchList(newFoundItem);
-      let newFoundImage = await data.artists.items[0].images[1].url;
-      setImageSrc(newFoundImage);
     } catch (err) {
       console.log(err);
     }
   }
 
-  function handleSearchClick(item) {
+  async function handleSearchClick(item) {
     setShowStar(item);
+
+    try {
+      let res = await fetch(
+        `https://api.spotify.com/v1/search?q=${searchMusic}&type=artist&market=US&offset=5&limit=10`,
+        {
+          headers: {
+            Authorization:
+              "Bearer BQAY6kZzsYqqgdxPVUO4WTH6ApovTUNc2Ujp-VAHdOpRHl18SNVKR6WlUTlo20GwdNfF1chZoxcVyDRtvjKQ3eGfH4l2GE2Mz7LjeOja9N7hQnBRbv-ulKAc33li8YvfkH_-BJ0MBPZm7pOT"
+          }
+        }
+      );
+      let data = await res.json();
+      let newFoundImage = await data.artists.items[0].images[1].url;
+      let url = await data.artists.items[0].external_urls.spotify;
+      setImageSrc(newFoundImage);
+      setLinkSrc(url);
+      console.log(url);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  //   function handleBackClick() {
-  //     setShowStar(false);
-  //   }
+  function handleBackClick() {
+    setShowStar(false);
+  }
+
+  if (showStar) {
+    return (
+      <div onClick={handleBackClick}>
+        <h2>{searchList}</h2>
+        {imageSrc && <img alt={searchList} src={imageSrc} />}
+        <br />
+        <a className="btn btn-success" href={linkSrc}>
+          {searchList}
+        </a>
+      </div>
+    );
+  }
 
   return (
     <div className="form-group row">
-      <div className="col-md-4" />
-      <div className="form-group row col-md-4">
-        <label htmlFor="inputMusic" className="col-md-3 col-form-label">
+      <div className="col-2" />
+      <div className="form-group row col-8">
+        <label htmlFor="inputMusic" className="col-2 col-form-label">
           Search
         </label>
         <input
           onChange={handleInputChange}
-          className="form-control col-md-9 dropdown-toggle"
+          className="form-control col-4"
           name="inputMusic"
           type="text"
         />
-        <br />
-        <br />
-        <br />
-        <div className="form-group row justify-content-right align-items-center">
-          <div className="col-9" onClick={handleSearchClick}>
+        <div className="row col-6">
+          <div className="col" onClick={handleSearchClick}>
             {searchList}
           </div>
-          <img id="picSize" className="col-3" alt={searchList} src={imageSrc} />
+          <img id="picSize" className="col" alt={searchList} src={imageSrc} />
         </div>
       </div>
-      <div className="col-md-4" />
+
+      <div className="col-2" />
     </div>
   );
 }
