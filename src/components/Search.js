bfb9754aa88bd7daa4a6593d138cf5632822a906
Vitaly Chemezov <vitaly.chemezov@gmail.com>
2019-07-15 10:23:38 +0200

    fix bugs

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Search(props) {
-  const [searchMusic, setSearchMusic] = React.useState(null);
+  const [searchMusic, setSearchMusic] = React.useState("");
   const [searchList, setSearchList] = React.useState("");
   const [showStar, setShowStar] = React.useState("");
   const [imageSrc, setImageSrc] = React.useState("");
@@ -10,37 +10,39 @@ function Search(props) {
   async function handleInputChange(event) {
     try {
       const value = event.target.value;
-      if (value) {
-        setSearchMusic(value);
-      }
+
+      setSearchMusic(value);
 
       let res = await fetch(
-        `https://api.spotify.com/v1/search?q=${searchMusic}&type=artist&market=US&offset=20&limit=10`,
+        `https://api.spotify.com/v1/search?q=${searchMusic}&type=artist&market=US&offset=0&limit=50`,
         {
           headers: {
             Authorization:
-              "Bearer BQDrFHTkDFEchBZbPIP4tuU4WouMG2Xgf0vrnNNlohLXncZZ84_W0iMchbKAg12jFoELL_XTP2utMyoxIBM2voBlSA-LMQ74EyMDGzDU9lWsJWQRwahfi8qtrjk2Py2wiY6wC7ocwXxBt1aa"
+              "Bearer BQA376iFIFUaI-13royC3Z6jMZtfN0KNjiTgOw_QrNMOJSidf6SsFi-BhHYvu5LjLD7n4ba83yjbVM0WSboZYbZjICtoyBA0r2Fsk3DoQhicwT864nRuL1uE8NECeh5EBtH89PsMLwu6Ut8B"
           }
         }
       );
       let data = await res.json();
       const newFoundItem = data.artists.items[0].name;
-      if (newFoundItem) {
-        setSearchList(newFoundItem);
-      }
+      setSearchList(newFoundItem);
     } catch (err) {
       console.log(err);
     }
   }
 
-  async function handleSearchClick(event) {
+  // React.useEffect(() => {
+  //   handleInputChange();
+  // }, []);
+
+  async function handleSearchClick(searchList) {
+    setShowStar(searchList);
     try {
       let res = await fetch(
-        `https://api.spotify.com/v1/search?q=${searchMusic}&type=artist&market=US&offset=20&limit=10`,
+        `https://api.spotify.com/v1/search?q=${searchMusic}&type=artist&market=US&offset=0&limit=50`,
         {
           headers: {
             Authorization:
-              "Bearer BQDrFHTkDFEchBZbPIP4tuU4WouMG2Xgf0vrnNNlohLXncZZ84_W0iMchbKAg12jFoELL_XTP2utMyoxIBM2voBlSA-LMQ74EyMDGzDU9lWsJWQRwahfi8qtrjk2Py2wiY6wC7ocwXxBt1aa"
+              "Bearer BQA376iFIFUaI-13royC3Z6jMZtfN0KNjiTgOw_QrNMOJSidf6SsFi-BhHYvu5LjLD7n4ba83yjbVM0WSboZYbZjICtoyBA0r2Fsk3DoQhicwT864nRuL1uE8NECeh5EBtH89PsMLwu6Ut8B"
           }
         }
       );
@@ -48,20 +50,18 @@ function Search(props) {
       let newFoundImage = await data.artists.items[0].images[1].url;
       let url = await data.artists.items[0].external_urls.spotify;
 
-      if (newFoundImage) {
-        setImageSrc(newFoundImage);
-      }
-      if (url) {
-        setLinkSrc(url);
-      }
-      if (searchList) {
-        setShowStar(searchList);
-      }
-      console.log(url);
+      setImageSrc(newFoundImage);
+      setLinkSrc(url);
+
+      //console.log(url);
     } catch (err) {
       console.log(err);
     }
   }
+
+  // React.useEffect(() => {
+  //   handleSearchClick();
+  // }, []);
 
   function handleBackClick() {
     setShowStar(false);
@@ -73,11 +73,10 @@ function Search(props) {
 
   if (showStar) {
     return (
-      <div onClick={handleBackClick}>
+      <div className="form-group" onClick={handleBackClick}>
         <h2>{searchList}</h2>
         <br />
         {imageSrc && <img id="picSize" alt={searchList} src={imageSrc} />}
-        <br />
         <br />
         <br />
         <div className="row justify-content-center">
