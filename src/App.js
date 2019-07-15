f5dd03e157f010022c0afda5d9548346eea5e5d2
Vitaly Chemezov <vitaly.chemezov@gmail.com>
2019-07-15 02:08:02 +0200

    remove header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Search from "./components/Search";
-//import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Favourite from "./components/Favourite";
 import { getFavFromStorage, setFavFromStorage } from "./utils/storage";
 import Footer from "./components/Footer";
-//import Header from "./components/Header";
 import Logo from "./components/Logo";
-import "./index.css";
 
 function App() {
   const [fav, setFav] = React.useState(getFavFromStorage());
--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Header(props) {
-  return <h3>{props.name}</h3>;
-}
-
-export default Header;
